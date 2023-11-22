8420a3a3e6257b7197309119bd57157b8d2795ec
Logan Wood <lgnwood@gmail.com>
2023-11-22 09:09:04 -0700

    complete tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let addToy = false;
 
+document.querySelector('.add-toy-form').addEventListener('submit', handleSubmit)
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -13,3 +15,68 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let toyObj = {
+    name:e.target.name.value,
+    image:e.target.image.value,
+    likes: 0
+  }
+  renderOneToy(toyObj)
+  addOneToy(toyObj)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getAllToys()
+})
+
+function getAllToys() {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(data => data.forEach(toy => renderOneToy(toy)))
+}
+
+function renderOneToy(toys) {
+  let card = document.createElement('div')
+  card.className = 'card'
+  card.innerHTML = `
+      <h2>${toys.name}</h2>
+      <img src="${toys.image}" class="toy-avatar" />
+      <p>
+        <span>${toys.likes}</span> Likes
+      </p>
+      <button class="like-btn" id="${toys.id}">Like</button> 
+  `
+  card.querySelector('.like-btn').addEventListener('click', () => {
+    toys.likes+= 1
+    card.querySelector('span').textContent = toys.likes
+    updateLikes(toys)
+  })
+
+  document.querySelector('#toy-collection').appendChild(card)
+}
+
+function addOneToy(toyObj) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body:JSON.stringify(toyObj)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy))
+}
+function updateLikes(toyObj) {
+  fetch(`http://localhost:3000/toys/${toyObj.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toyObj)
+  })
+  .then(res => res.json())
+  
+}
